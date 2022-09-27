893805ffb374443722cd6db25389c622a037cc06
EnverJ <Enlem1987@gmail.com>
2022-09-26 22:04:33 -0600

    loop

---
+++ Documents/JavaScrip/doWhileLoop_7.js
@@ -0,0 +1,23 @@
+// 1 to 10
+// let i=1;
+// do
+// {
+//     console.log(i);
+//     i++;
+// }while(i<11);
+
+// at least executed once
+// let i=100;
+// do
+// {
+//     console.log(i);
+//     i++;
+// }while(i<11);
+
+// descending
+let i=10;
+do
+{
+    console.log(i);
+    i--;
+}while(i>0);
