542951e10173cb1c11a3fbe2022efe2bf295dc95
EnverJ <Enlem1987@gmail.com>
2022-09-28 22:27:00 -0600

    loop

---
+++ Documents/JavaScrip/forLoop_7.js
@@ -0,0 +1,19 @@
+// print 1 to 10
+
+for(let i=1;i<11;i++)
+{
+console.log(i);
+}
+
+// print even
+
+for(let a=2;a<11;a+=2)
+{
+console.log(a);
+}
+
+// desending
+for(let j=10;j>0;j--)
+{
+console.log(j);
+}
