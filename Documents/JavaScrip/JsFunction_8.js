65c56dcb5c54a8dee58b0b08303c876f766226f9
EnverJ <Enlem1987@gmail.com>
2022-09-29 22:51:38 -0600

    function

---
+++ Documents/JavaScrip/JsFunction_8.js
@@ -0,0 +1,30 @@
+// function add(a,b)
+// {
+// console.log(a+b);
+// }
+
+// add(2,900);
+
+//Example function return value
+// function add(a,b)
+// {
+// return (a+b);
+// }
+// let c=add(200,89);
+// console.log(c);
+
+//Example 3
+//  function greeting()
+//  {
+//     console.log(" Good mornign js");
+//  }
+
+//  greeting();
+
+//Example 4
+function greetings()
+{
+    return ("Good morning js");
+}
+let msg=greetings();
+console.log(msg);
---
+++ Documents/JavaScrip/controlStatements_6.js
@@ -0,0 +1,7 @@
+// 1. Conditional Statements
+// if, else if, Switch case
+
+
+
+// looping statements
+//Jumping Statements
