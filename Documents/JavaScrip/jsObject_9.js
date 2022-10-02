00c133c9305c80b452a9dd091ec4d7fa01b10bdf
EnverJ <Enlem1987@gmail.com>
2022-10-01 21:26:34 -0600

    object

---
+++ Documents/JavaScrip/jsObject_9.js
@@ -0,0 +1,13 @@
+ let person=
+ {
+    firstName:"John",
+    lastName:"Jordan",
+    age:45,
+    weight:167
+
+ }
+
+ //Acees object property
+ console.log(person["firstName"]);
+ console.log(person["lastName"]);
+ console.log(person["age"]);
