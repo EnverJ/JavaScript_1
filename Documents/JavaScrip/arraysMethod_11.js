623d3c69940a3187598080eea9f5d7d09dc1845d
EnverJ <Enlem1987@gmail.com>
2022-10-06 22:30:41 -0600

    array methods

--- Documents/JavaScrip/arraysMethod_11.js
+++ Documents/JavaScrip/arraysMethod_11.js
@@ -22,3 +22,43 @@ console.log(fruits);
 fruits=["Orange","Banana","Apple","Mango"];
 console.log(fruits.shift());
 console.log(fruits);
+
+//unshift   add new element at the begining
+fruits=["Orange","Banana","Apple","Mango"];
+//fruits.unshift("lemon");
+console.log(fruits.unshift("lemon"));
+
+// delete elements from array
+fruits=["Orange","Banana","Apple","Mango"];
+delete fruits[1];
+console.log(fruits);
+
+// concat()---concotinate, join/merging 2 or more arays
+let arr1=[10,20];
+let arr2=["A","B","C"];
+console.log(arr1.concat(arr2));
+
+let arr3=["x","y","z"];
+console.log(arr1.concat(arr2,arr3));
+
+//slice()  slcile elemets from an array
+fruits=["Orange","Banana","Apple","Mango","lemon"];
+console.log(fruits.slice(1));  //[ 'Banana', 'Apple', 'Mango', 'lemon' ]
+console.log(fruits.slice(2));   //[ 'Apple', 'Mango', 'lemon' ]
+
+
+// sort()   
+fruits=["Orange","Banana","Apple","Mango","lemon"];
+console.log(fruits.sort());    //[ 'Apple', 'Banana', 'Mango', 'Orange', 'lemon' ]
+
+let nums=[34,56,456,902];
+console.log(nums.sort());  //[ 34, 456, 56, 902 ]
+
+// reverse()
+fruits=["Orange","Banana","Apple","Mango","lemon"];
+console.log("Original arraya element "+fruits)
+fruits.reverse();
+console.log("after reversing elements of fruits array are "+fruits);
+
+
+
